d4b2ee10042c1b9838824f83ce389f279e5ac179
lh632861400 <632861400@qq.com>
2020-09-28 15:23:37 +0800

    test: add SingleCircleLinkedList `size`

--- data-structure/LinkedList/single/__test__/SingleCircleLinkedList.test.ts
+++ data-structure/LinkedList/single/__test__/SingleCircleLinkedList.test.ts
@@ -1,6 +1,18 @@
 import {describe, expect, test} from '@jest/globals'
-import SingleNoCircleLinkedList from "../SingleNoCircleLinkedList";
+import SingleCircleLinkedList from "../SingleCircleLinkedList";
 
 describe('SingleCircleLinkedList', () => {
-  
+
+  test("size equal 5", () => {
+    const linkedList = new SingleCircleLinkedList<Number>();
+    linkedList.add(11);
+    linkedList.add(22);
+    linkedList.add(33);
+    linkedList.add(44);
+    linkedList.add(0, 1);
+    linkedList.add(5, 123213);
+
+    expect(linkedList.getSize()).toBe(6)
+  });
+
 });
