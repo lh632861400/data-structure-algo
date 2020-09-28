32acebc47fe911a375d2ccde2c16ef5b8bd23788
lh632861400 <632861400@qq.com>
2020-09-28 15:25:34 +0800

    test: add SingleCircleLinkedList method

--- data-structure/LinkedList/single/__test__/SingleCircleLinkedList.test.ts
+++ data-structure/LinkedList/single/__test__/SingleCircleLinkedList.test.ts
@@ -1,5 +1,6 @@
 import {describe, expect, test} from '@jest/globals'
 import SingleCircleLinkedList from "../SingleCircleLinkedList";
+import SingleNoCircleLinkedList from "../SingleNoCircleLinkedList";
 
 describe('SingleCircleLinkedList', () => {
 
@@ -14,5 +15,29 @@ describe('SingleCircleLinkedList', () => {
 
     expect(linkedList.getSize()).toBe(6)
   });
+
+  test("contains 11", () => {
+    const linkedList = new SingleNoCircleLinkedList<Number>();
+    linkedList.add(11);
+    linkedList.add(22);
+    linkedList.add(33);
+    linkedList.add(44);
+    linkedList.add(1);
+
+    expect(linkedList.contains(11)).toBe(true);
+    expect(linkedList.contains(2)).toBe(false);
+  })
+
+  test("contains getElement From index equals 2 return 33", () => {
+    const linkedList = new SingleNoCircleLinkedList<Number>();
+    linkedList.add(11);
+    linkedList.add(22);
+    linkedList.add(33);
+    linkedList.add(44);
+    linkedList.add(1);
+
+    expect(() => { linkedList.getElement(11) }).toThrow();
+    expect(linkedList.getElement(2)).toBe(33)
+  })
 
 });
