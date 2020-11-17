96c97bbd6c89fb16151eaa5c2f01f6be8c4f1bba
lh632861400 <632861400@qq.com>
2020-11-17 17:46:54 +0800

    feat: add leetcode-1030

---
+++ leetcode/BinarySearchTree/1030. 距离顺序排列矩阵单元格/2.js
@@ -0,0 +1,79 @@
+/**
+ *
+ * thoughts:
+ * 采用层序遍历的方法
+ * 将[r0, c0]所在坐标作为根节点，距离[r0, c0]曼哈顿距离为1的作为子节点
+ * 同样递归子节点
+ *
+ * perf:
+ * 采用数组解决记忆化的问题
+ * O(R * C )
+ *
+ *
+ * @param {number} R
+ * @param {number} C
+ * @param {number} r0
+ * @param {number} c0
+ * @return {number[][]}
+ */
+var allCellsDistOrder = function(R, C, r0, c0) {
+
+  // 记录已经输出的坐标节点
+  const resultsMap = new Array(R)
+  for(let i = 0; i < R; i++) {
+    resultsMap[i] = new Array(C);
+  }
+
+  const queue = [];
+
+  queue.push([r0, c0]);
+
+  const results = [];
+
+  resultsMap[r0][c0] = true;
+
+  // 当队列中还有数据
+  while(queue.length) {
+
+    const node = queue.shift();
+    const r = node[0];
+    const c = node[1];
+
+    results.push(node);
+
+    // [r, c]坐标为r >= 0 && r < R && c >= 0 && c < C的合法坐标放入到队列中周围的数据放入到队列
+    if(r - 1 >= 0) {
+
+      if(!resultsMap[r - 1][c]) { // 判断节点左边数据是否合法并且未输出
+        queue.push([r - 1, c])
+        resultsMap[r - 1][c] = true;
+      }
+    }
+
+    if(r + 1 < R) {
+      if(!resultsMap[r + 1][c]) { // 判断节点右边数据是否合法并且未输出
+        queue.push([r + 1, c])
+        resultsMap[r + 1][c] = true;
+      }
+    }
+
+    if(c - 1 >= 0) {
+
+      if(!resultsMap[r][c - 1]) { // 判断节点左边数据是否合法并且未输出
+        queue.push([r, c - 1])
+        resultsMap[r][c - 1] = true;
+      }
+
+    }
+
+    if(c + 1 < C) {
+      if(!resultsMap[r][c + 1]) { // 判断节点左边数据是否合法并且未输出
+        queue.push([r, c + 1])
+        resultsMap[r][c + 1] = true;
+      }
+    }
+  }
+
+  return results;
+
+};
---
+++ leetcode/BinarySearchTree/1030. 距离顺序排列矩阵单元格/3.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * thoughts:
+ * 对所有点的曼哈顿距离进行排序
+ * 0(RClogRC)
+ * O(1)
+ *
+ *
+ * @param {number} R
+ * @param {number} C
+ * @param {number} r0
+ * @param {number} c0
+ * @return {number[][]}
+ */
+var allCellsDistOrder = function(R, C, r0, c0) {
+
+};
